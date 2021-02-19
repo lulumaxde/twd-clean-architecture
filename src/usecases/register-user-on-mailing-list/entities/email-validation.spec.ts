c72d7acdea2bae7c370c8ba0b21299f056de9909
Lucas de Jesus <lucas_moreira110@hotmail.com>
2021-02-19 19:32:51 -0300

    test: validate local part larger than 64 chars

--- src/usecases/register-user-on-mailing-list/entities/email-validation.spec.ts
+++ src/usecases/register-user-on-mailing-list/entities/email-validation.spec.ts
@@ -15,4 +15,9 @@ describe('Email Validation', () => {
     const email = 'any@mail.com'
     expect(Email.validate(email)).toBeTruthy()
   })
+
+  test('should not accept local part larger than 64 chars', () => {
+    const email = 'l'.repeat(65) + '@mail.com'
+    expect(Email.validate(email)).toBeFalsy()
+  })
 })
