f069543a7376d86d6365e89c0f6dd86aa7602794
Lucas de Jesus <lucas_moreira110@hotmail.com>
2021-02-19 20:48:32 -0300

    test: validate email with domain part invalue char

--- src/usecases/register-user-on-mailing-list/entities/email-validation.spec.ts
+++ src/usecases/register-user-on-mailing-list/entities/email-validation.spec.ts
@@ -45,4 +45,9 @@ describe('Email Validation', () => {
     const email = 'any@' + 'd'.repeat(64) + '.com'
     expect(Email.validate(email)).toBeFalsy()
   })
+
+  test('should not accept local part with invalid char', () => {
+    const email = 'any email@.com'
+    expect(Email.validate(email)).toBeFalsy()
+  })
 })
