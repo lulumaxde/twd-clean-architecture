a0db6f51e7611069e95b4d36ff4e7fb956b4e39d
Lucas de Jesus <lucas_moreira110@hotmail.com>
2021-03-09 00:29:45 -0300

    refactor: move tests to their owner folder

---
+++ test/Entities/user.spec.ts
@@ -0,0 +1,34 @@
+import { User } from '../../src/usecases/register-user-on-mailing-list/entities/user'
+
+
+describe('User domain class', () => {
+  test('should not create user with invalid email adress', () => {
+    const invalidEmail = 'invalid_email'
+    const error = User.create({ name: 'any_name', email: invalidEmail }).value as Error
+    expect(error.name).toEqual('InvalidEmailError')
+    expect(error.message).toEqual('Invalid email: ' + invalidEmail + '.')
+  })
+
+  test('should not create user with invalid name(too few characters', () => {
+    const invalidName = 'o        '
+    const error = User.create({ name: invalidName, email: 'any@mail.com' }).value as Error
+    expect(error.name).toEqual('InvalidNameError')
+    expect(error.message).toEqual('Invalid name: ' + invalidName + '.')
+  })
+
+  //test('should not create user with invalid name(too many characters', () => {
+    //const invalidName = 'o'.repeat(257)
+    //const error = User.create({ name: invalidName, email: 'any@mail.com' })
+    //expect(error).toEqual(left(new InvalidNameError()))
+  //})
+
+  test('should create user with valid data', () => {
+    const validName = 'any_name'
+    const validEmail = 'any@mail.com'
+    const user: User = User.create( {name: validName, email: validEmail }).value as User
+    expect(user.name.value).toEqual(validName)
+    expect(user.email).toEqual(validEmail)
+    
+  })
+
+})
--- test/usecases register user on mailing list/repository/in-memory-user-repository.spec.ts
+++ test/usecases register user on mailing list/repository/in-memory-user-repository.spec.ts
@@ -1,4 +1,4 @@
-import { UserData } from '../entities/user-data'
+import { UserData } from '../../../src/usecases/register-user-on-mailing-list/entities/user-data'
 import { InMemoryUserRepository } from './in-memory-user-repository'
 
 describe('In memory User Repository', () => {
